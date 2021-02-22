870d0097fe5b45d2916fe47d2943af8c26bf3a48
ianmcnicholas <ianjmcnicholas@gmail.com>
2021-02-22 17:07:01 +0000

    Viewing Bookmarks Ian and Jai

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
-
+require './lib/bookmark.rb'
 require 'sinatra/base'
 
 # Store and display Bookmarks
 class BookmarkManager < Sinatra::Base
   get '/' do
     erb(:index)
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb(:bookmarks)
   end
 
   run! if app_file == BookmarkManager
